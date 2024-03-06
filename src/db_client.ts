01d5b1da52f91497391c5b62c19f78203aef1481
jurgob <jurgob@users.noreply.github.com>
2024-03-06 13:51:58 +0000

    enable real mongo

--- src/db_client.ts
+++ src/db_client.ts
@@ -10,22 +10,29 @@ type User= {
 
 export async function  createDBClient(){
 
-  // const useMock = env.NODE_ENV === 'test';
-  const mongoServer = await MongoMemoryServer.create();
-  
-  const mongo_db_name = `db_${Math.random().toString(36).substring(7)}`;
-  const mongo_url = mongoServer.getUri()+`/${mongo_db_name}`
+  let mongoServer:MongoMemoryServer|undefined =undefined;
+  let mongo_db_name: string| undefined = undefined;
+  let mongo_url: string| undefined = undefined;
+  if(!env.MONGO_URL){
+    mongoServer = await MongoMemoryServer.create()
+    mongo_db_name = `db_${Math.random().toString(36).substring(7)}`;
+    mongo_url = mongoServer.getUri()+`/${mongo_db_name}`
+  }else {
+    if(!env.MONGO_DB_NAME){
+      throw new Error('MONGO_DB_NAME is required when using a real mongodb server (aka when MONGO_URL is defined)');
+    }
+    mongo_url = `${env.MONGO_URL}/${env.MONGO_DB_NAME}`;
+    mongo_db_name = env.MONGO_DB_NAME;
+  }  
 
   const db = new MongoClient(mongo_url);
   const collectionUsers = db.db(mongo_db_name).collection<User>('users');
 
-
-
-  
-
   const disconnect = async () => {
     await db.close();
-    await mongoServer.stop();
+    
+    if(mongoServer)
+      await mongoServer.stop();
     
   }
 
--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
     PORT: z.coerce.number().min(1000).default(3000),
     TEST_API_URL: z.string().url().optional(),//if empty, the test will use an inmemory-mock server
     MONGO_URL: z.string().url().optional(),// if empty, the express server will use an inmemory-mock mongodb version
+    MONGO_DB_NAME: z.string().max(50).optional(),
 });
 
 const env = envSchema.parse(process.env)
